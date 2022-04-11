da4eacd531f21e4fdbfb0af1bf70981a8138a89b
mbiciunas@gmail.com <73kObaAq3E4R>
2022-04-11 16:51:28 -0400

    Update for API to work with cors.

--- scrunt.go
+++ scrunt.go
@@ -29,7 +29,7 @@ func main() {
 	}
 
 	// Start and run the server
-	err = router.Run(":3000")
+	err = router.Run(":8080")
 	if err != nil {
 		return
 	}
--- startup/router.go
+++ startup/router.go
@@ -21,6 +21,7 @@ func router() *gin.Engine {
 func cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
