087a6b3ee6aec3a0975faab19d48fb955a981d50
mbiciunas@gmail.com <73kObaAq3E4R>
2022-11-08 10:16:13 -0500

    add python runner file to call script.

--- python/python_exec.go
+++ python/python_exec.go
@@ -3,54 +3,148 @@ package python
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
-	//"github.com/go-python/cpy3"
 	"log"
 	"os/exec"
+	"scrunt-back/startup"
 	"strconv"
 )
 
-func Exec(id int, code string) {
-	//scriptStart := ""
-	scriptStart := "from libscrunt import Scrunt\n"
-	scriptStart += "scrunt = Scrunt(" + strconv.Itoa(id) + ")\n\n"
-	//scriptStart := "import sys\nsys.stdout = open('Output.txt', 'w')\n"
-	//scriptEnd := "sys.stdout.close()"
-	script := scriptStart + "\n" + code
-	fmt.Println("RunId: ", id, "Script: ", script)
-	//fmt.Println("Script: ", code)
-
-	//err := python3.PySys_SetArgv([]string{strconv.Itoa(id)})
-	//if err != nil {
-	//	return
-	//}
+func Exec(runId int, code string) {
+	//
+	// Create runtime directory
+	//
+	createRuntimeDir()
 
 	//
-	// Create temp file in local directory
+	// Create Run Directory
 	//
-	f, err := os.CreateTemp(".", "*.py")
+	runDirectory := createRunDir()
+
+	//defer os.RemoveAll(runDirectory)
+
+	//
+	// Create python program to run script
+	//
+	runFile := createPythonRunner(runDirectory, runId)
+
+	//
+	// Save script file in run directory
+	//
+	createPythonScript(runDirectory, code)
+
+	//
+	// Save script file in run directory
+	//
+	createPythonPackage(runDirectory)
+
+	cmd := exec.Command(".scrunt/python/bin/python3", runFile)
+	out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatalf("Failed to create temp file", err)
+		log.Printf("exec.Command() failed with %s\n\n%s", err, out)
 	}
-	fmt.Println("Temp file name:", f.Name())
-	defer os.Remove(f.Name())
+}
+
+func createRuntimeDir() {
+	err := os.MkdirAll(startup.GetDirectoryRuntime(), os.ModePerm)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Current runtime directory:", startup.GetDirectoryRuntime())
+}
+
+func createRunDir() string {
+	runDir, err := os.MkdirTemp(startup.GetDirectoryRuntime(), "run-*")
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Run directory:", runDir)
+
+	return runDir
+}
+
+func createPythonRunner(runDirectory string, runId int) string {
+	scriptFile := filepath.Join(runDirectory, "runner.py")
+
+	//
+	// Create Runner file in run directory
+	//
+	f, err := os.Create(scriptFile)
+	if err != nil {
+		log.Fatalf("Failed to create runner file: %s", err)
+	}
+	fmt.Println("Runner file name:", f.Name())
+
+	script := "from libscrunt.scrunt import Scrunt\n"
+	script += "import runner_logging\n"
+	script += "\n"
+	script += "_scrunt = Scrunt(" + strconv.Itoa(runId) + ")\n"
+	script += "\n"
+	script += "try:\n"
+	script += "    runner_logging.main(_scrunt)\n"
+	script += "except SystemExit as systemExit:\n"
+	script += "    _scrunt.log().error(\"System exit - {}\".format(systemExit.args))\n"
+	script += "except KeyboardInterrupt as keyboardInterrupt:\n"
+	script += "    _scrunt.log().error(\"Keyboard Interrupt - {}\".format(keyboardInterrupt.args))\n"
+	script += "except Exception as exception:\n"
+	script += "    _scrunt.log().critical(\"This is a critical exception\")\n"
 
 	_, err = f.Write([]byte(script))
 	if err != nil {
-		log.Fatalf("Failed to write to temp file", err)
+		log.Fatalf("Failed to write runner file: %s", err)
 	}
 
-	fmt.Println("Before Exec")
-	//output := python3.PyRun_SimpleString(script)
-	//cmd := exec.Command("ls", "-lah")
-	//cmd := exec.Command(".scrunt/python3/bin/python3", "-I", f.Name())
-	cmd := exec.Command(".scrunt/python/bin/python3", f.Name())
-	out, err := cmd.CombinedOutput()
+	err = f.Close()
 	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n\n%s", err, out)
+		log.Fatalf("Failed to close runner file: %s", err)
 	}
-	//fmt.Printf("combined out:\n%s\n", string(out))
-	//fmt.Println("After Exec", output)
+
+	return scriptFile
 }
 
-//python -I -c "commands"
+func createPythonScript(runDirectory string, code string) string {
+	scriptFile := filepath.Join(runDirectory, "runner_logging.py")
+
+	//
+	// Create script file in run directory
+	//
+	f, err := os.Create(scriptFile)
+	if err != nil {
+		log.Fatalf("Failed to create script file: %s", err)
+	}
+	fmt.Println("Script file name:", f.Name())
+
+	_, err = f.Write([]byte(code))
+	if err != nil {
+		log.Fatalf("Failed to write to script file: %s", err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		log.Fatalf("Failed to close script file: %s", err)
+	}
+
+	return scriptFile
+}
+
+func createPythonPackage(runDirectory string) {
+	packageFile := filepath.Join(runDirectory, "__init__.py")
+
+	//
+	// Create package file in run directory
+	//
+	f, err := os.Create(packageFile)
+	if err != nil {
+		log.Fatalf("Failed to create package file: %s", err)
+	}
+	fmt.Println("Package file name:", f.Name())
+
+	err = f.Close()
+	if err != nil {
+		log.Fatalf("Failed to close package file: %s", err)
+	}
+}
