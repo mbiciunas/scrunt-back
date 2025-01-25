031c6c52b2921fc06231763af2818359a7241619
mbiciunas@gmail.com <73kObaAq3E4R>
2025-01-25 12:51:46 -0500

    Add tables to SQLite database build

--- build/sqlite/build.go
+++ build/sqlite/build.go
@@ -5,16 +5,19 @@ import (
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"os"
 	"time"
 )
 
 type Service struct {
-	Id            uint   `json:"id" gorm:"primarykey"`
-	Name          string `json:"name"`
-	Description   string `json:"description"`
-	Address       string `json:"address"`
-	Port          int    `json:"description_long"`
-	ServiceTypeId uint   `json:"service_type_id"`
+	Id              uint             `json:"id" gorm:"primarykey"`
+	IconId          uint             `json:"icon_id"`
+	Name            string           `json:"name"`
+	Description     string           `json:"description"`
+	Address         string           `json:"address"`
+	Port            int              `json:"description_long"`
+	ServiceTypeId   uint             `json:"service_type_id"`
+	VersionServices []VersionService `gorm:"foreignkey:ServiceId"`
 }
 
 type ServiceType struct {
@@ -34,29 +37,91 @@ type ScriptServiceType struct {
 
 type Script struct {
 	Id                 uint                `json:"id" gorm:"primarykey"`
+	IconId             uint                `json:"icon_id"`
 	Name               string              `json:"name"`
-	Description        string              `json:"description"`
 	DescriptionShort   string              `json:"description_short"`
 	DescriptionLong    string              `json:"description_long"`
 	Code               string              `json:"code"`
 	ScriptServiceTypes []ScriptServiceType `gorm:"foreignkey:ScriptId"`
 	Versions           []Version           `gorm:"foreignkey:ScriptId"`
+	ScriptTag          []ScriptTag         `gorm:"foreignkey:ScriptId"`
 }
 
 type Version struct {
-	Id       uint      `json:"id" gorm:"primarykey"`
-	ScriptId uint      `json:"script_id"`
-	Created  time.Time `json:"service_type_id"`
-	Major    int64     `json:"major"`
-	Minor    int64     `json:"minor"`
-	Patch    int       `json:"patch"`
-	Commit   int       `json:"commit"`
-	Uuid     string    `json:"uuid"`
-	Contents string    `json:"changes"`
+	Id              uint             `json:"id" gorm:"primarykey"`
+	ScriptId        uint             `json:"script_id"`
+	Created         time.Time        `json:"service_type_id"`
+	Major           int64            `json:"major"`
+	Minor           int64            `json:"minor"`
+	Patch           int              `json:"patch"`
+	Commit          int              `json:"commit"`
+	Uuid            string           `json:"uuid"`
+	Contents        string           `json:"changes"`
+	VersionCodes    []VersionCode    `gorm:"foreignkey:VersionId"`
+	VersionServices []VersionService `gorm:"foreignkey:VersionId"`
+}
+
+type Code struct {
+	Id           uint          `json:"id" gorm:"primarykey"`
+	Type         string        `json:"type"`
+	Value        string        `json:"value"`
+	Uuid         string        `json:"uuid"`
+	VersionCodes []VersionCode `gorm:"foreignkey:CodeId"`
+}
+
+type VersionCode struct {
+	Id        uint `json:"id" gorm:"primarykey"`
+	VersionId uint `json:"version_id"`
+	CodeId    uint `json:"code_id"`
+}
+
+type VersionService struct {
+	Id         uint   `json:"id" gorm:"primarykey"`
+	VersionId  uint   `json:"version_id"`
+	ServiceId  uint   `json:"service_id"`
+	VersionMin uint   `json:"version_min"`
+	VersionMax uint   `json:"version_max"`
+	Usage      string `json:"usage"`
+	Details    string `json:"details"`
+}
+
+type TagType struct {
+	Id          uint   `json:"id" gorm:"primarykey"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Tag         []Tag  `gorm:"foreignkey:TagTypeId"`
+}
+
+type Tag struct {
+	Id        uint        `json:"id" gorm:"primarykey"`
+	TagTypeId uint        `json:"tag_type_id"`
+	Name      string      `json:"name"`
+	ScriptTag []ScriptTag `gorm:"foreignkey:TagId"`
+}
+
+type ScriptTag struct {
+	Id       uint `json:"id" gorm:"primarykey"`
+	ScriptId uint `json:"script_id"`
+	TagId    uint `json:"tag_id"`
+}
+
+type Icon struct {
+	Id        uint    `json:"id" gorm:"primarykey"`
+	Code      uint    `json:"script_id"`
+	Directory string  `json:"directory"`
+	Filename  string  `json:"filename"`
+	Service   Service `gorm:"foreignkey:IconId"`
+	Script    Script  `gorm:"foreignkey:IconId"`
 }
 
 func main() {
-	fmt.Println("Build Sqlite")
+	fmt.Println("Delete gorm.db")
+	err := os.Remove("./gorm.db")
+	if err != nil {
+		panic("Failed to delete the SQLite database.")
+	}
+
+	fmt.Println("Open Sqlite database")
 	// Open a new connection to our sqlite database.
 	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 	if err != nil {
@@ -64,7 +129,21 @@ func main() {
 	}
 
 	// Create the table from our struct.
-	err = db.AutoMigrate(&Script{}, &Service{}, &ServiceType{}, &ScriptServiceType{}, &Version{})
+	err = db.AutoMigrate(
+		&Script{},
+		&Service{},
+		&ServiceType{},
+		&ScriptServiceType{},
+		&Version{},
+		&Code{},
+		&VersionCode{},
+		&VersionService{},
+		&TagType{},
+		&Tag{},
+		&ScriptTag{},
+		&Icon{},
+	)
+
 	if err != nil {
 		panic("Failed to automigrate.")
 	}
@@ -72,7 +151,6 @@ func main() {
 	// Create a new user in our database.
 	db.Create(&Script{
 		Name:             "Script 1",
-		Description:      "Description 1",
 		DescriptionLong:  "Long Description 1",
 		DescriptionShort: "Short Description 1",
 		Code:             "This is my script code...",
